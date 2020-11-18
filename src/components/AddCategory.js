a8c08b7d93b69282d3d595cfe667d1c38a6612f7
Juan Manuel <jmcontrerasvega@gmail.com>
2020-11-17 22:06:05 -0500

    otros cambios

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -12,7 +12,8 @@ export const AddCategory = ({ setCategories }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if ( inputValue.trim().length > 2) {
-      inputValue && setCategories( (category) => [...category, inputValue] );
+      inputValue && setCategories( (category) => [ inputValue,...category,] );
+        setinputValue('');
     }
   };
 
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,48 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { GifGridItem } from "./GifGridItem";
-import "../index.css"
+import "../index.css";
+import { getGifs } from "../helpers/getGifs";
 export const GifGrid = ({ category }) => {
-  const [counter, setCounter] = useState(0);
+
   const [images, setImages] = useState([]);
+  
   useEffect(() => {
-    getGifs();
+    getGifs(category).then((img) => setImages(img));// eslint-disable-next-line
   }, []);
-
-  //Metodo que recorre una api
-  const getGifs = async () => {
-    const url =
-      `https://api.giphy.com/v1/gifs/search?api_key=9ICmnzq1JsbrjnsodRdeDHrub6iz52rX&q=${encodeURI(category)}&limit=25&offset=0&rating=g&lang=en`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-
-    console.log(gifs);
-    setImages(gifs);
-  };
 
   return (
     <>
       <h3>{category}</h3>
-    <div className="card-grid">
-      <h3>{counter}</h3>
-
-      {images.map((img) => (
-        <GifGridItem 
-        key={img.id} 
-        {...img}
-        
-        />
-      ))}
-
-      <button onClick={() => setCounter(counter + 1)}>Boton</button>
-    </div>
+      <div className="card-grid">
+        {/* <h3>{counter}</h3> */}
+        {images.map((img) => (<GifGridItem key={img.id} {...img} />))}
+        {/* <button onClick={() => setCounter(counter + 1)}>Boton</button> */}
+      </div>
     </>
   );
 };
-
